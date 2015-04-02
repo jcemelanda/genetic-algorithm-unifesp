fba575a0b456dea288aa3cec15be588865997874
Julio Cesar Melanda <jcemelanda@gmail.com>
2015-04-01 23:11:37 -0300

    Melhorias no algoritmo

--- traveler_salesman.py
+++ traveler_salesman.py
@@ -5,15 +5,13 @@ __author__ = 'julio'
 
 
 def drop_dupes(chromossome, reverse):
-    chromossome = list(chromossome)
+    dupe_chromossome = chromossome
     if reverse:
-        chromossome.reverse()
+        dupe_chromossome = chromossome[::-1]
     seen = set()
     seen_add = seen.add
-    chromossome = [x for x in chromossome if not (x in seen or seen_add(x))]
-    if reverse:
-        chromossome.reverse()
-    return chromossome
+    chromossome = [x for x in dupe_chromossome if not (x in seen or seen_add(x)) else -1]
+    return chromossome[::-1] if reverse else chromossome
 
 
 def crossover(parents, genes):
@@ -21,27 +19,37 @@ def crossover(parents, genes):
     result_chromossome = parents[0][:crossover_point]+parents[1][crossover_point:]
     reverse = choice((True, False))
     result_chromossome = drop_dupes(result_chromossome, reverse)
-    result_chromossome.extend(genes - set(result_chromossome))
+    if -1 in result_chromossome:
+        new_genes = list(genes - set(result_chromossome))
+        shuffle(new_genes)
+        while -1 in result_chromossome:
+            result_chromossome[result_chromossome.index(-1)] = new_genes.pop()
     return tuple(result_chromossome)
 
 
-def create_new_population(original_population, replacement_size, genes):
-    new_population = set()
+def create_new_population(original_population, replacement_size, genes, c_rate):
+    new_population = []
     gene_set = set(genes)
-    parents = original_population[:2]
+    parents = sample(original_population[:replacement_size],2)
     while len(new_population) < replacement_size:
-        if choice((True, False)):
-            parents.reverse()
-        new_population.add(crossover(parents, gene_set))
+        do_crossover = randrange(101) <= c_rate
+        if do_crossover:
+            if choice((True, False)):
+                parents.reverse()
+            new_population.append(crossover(parents, gene_set))
+        else:
+            new_population.append(choice(parents))
+    print(parents)
+    print(new_population[:5])
     original_population[:-replacement_size].extend(new_population)
     return set(original_population)
 
 
 def generate_first_population(genes, population_size):
-    population = set()
+    population = []
     while len(population) < population_size:
         shuffle(genes)
-        population.add(tuple(genes))
+        population.append(tuple(genes))
     return population
 
 
@@ -62,24 +70,22 @@ def order_by_fitness(evaluated_population):
     return ordered[0][1], [x[0] for x in ordered]
 
 
-def run(cities_matrix, population_size, replacement_size, generations):
+def run(cities_matrix, population_size, replacement, generations, cross_rate):
     genes = list(range(len(cities_matrix)))
     population = generate_first_population(genes, population_size)
     fitness = 0
     fitness_changed = True
     for i in range(generations):
         fitness_changed = False
-        new_fitness, population_list = order_by_fitness([get_fitness(x, cities_matrix) for x in population])
+        new_fitness, population_list = order_by_fitness(
+            [get_fitness(x, cities_matrix) for x in population])
         if new_fitness < fitness or not fitness:
             fitness = new_fitness
             fitness_changed = True
-        print(fitness)
-        population = create_new_population(population_list, replacement_size, genes)
-
+        population = create_new_population(population_list, replacement,
+            genes, cross_rate)
 
     return fitness, population_list[0]
-
-
 
 
 if __name__ == '__main__':
@@ -88,6 +94,8 @@ if __name__ == '__main__':
     parser.add_argument('matrix_file')
     parser.add_argument('population_size')
     parser.add_argument('replacement_size')
+    parser.add_argument('crossover_rate')
+    parser.add_argument('generations_number')
     args = parser.parse_args()
     filename = args.matrix_file
     f = open(filename, 'r')
@@ -98,4 +106,6 @@ if __name__ == '__main__':
 
     pop_size = int(args.population_size)
     rep_size = int(args.replacement_size)
-    print(run(matrix, pop_size, rep_size, 1000))
+    crossover_rate = int(args.crossover_rate)
+    generations = int(args.generations_number)
+    print(run(matrix, pop_size, rep_size, generations, crossover_rate))
